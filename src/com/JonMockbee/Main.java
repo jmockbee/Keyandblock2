8ab5dd0de1839e4e16e3a5e330dab77cf98ca016
Jon Mockbee <mockbee1000@gmail.com>
2019-07-05 13:26:41 -0400

    adding changes trying not to repeat vairables

--- src/com/JonMockbee/Main.java
+++ src/com/JonMockbee/Main.java
@@ -4,17 +4,32 @@ public class Main {
 
     public static void main(String[] args) {
         boolean gameover = true;
-        int score = 4000;
-        int levelCompleted = 5;
-        int bonus = 1000;
+        int score = 5000;
+        int levelCompleted = 10;
+        int bonus = 2000;
 
-        if (score < 5000 && score > 1000) {
+    /*    if (score < 5000 && score > 1000) {
             System.out.println("Your score was less than 5000");
         } else if (score < 1000) {
             System.out.println("your score was less than 1000");
         } else {
             System.out.println(" Got here");
-        }
+        }*/
+    if (gameover == true){
+        int finalScore = score + (levelCompleted * bonus);
+        System.out.println("Your final score was" + finalScore);
+ }
+//        if (gameover){
+//            int finalScore = score + (levelCompleted * bonus);
+//            System.out.println("Your final score was" + finalScore);
+//        }
+        score = 10000;
+        levelCompleted = 8;
+        bonus = 2000;
 
+        if (gameover == true){
+            int finalScore = score + (levelCompleted * bonus);
+            System.out.println("Your final score was" + finalScore);
+        }
     }
 }
