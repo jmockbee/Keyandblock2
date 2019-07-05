aa51cbd6a4471f6708cda540f0ae0a3265195d3b
Jon Mockbee <mockbee1000@gmail.com>
2019-07-05 11:09:23 -0400

    Blocking off if else staments

--- src/com/JonMockbee/Main.java
+++ src/com/JonMockbee/Main.java
@@ -4,11 +4,17 @@ public class Main {
 
     public static void main(String[] args) {
         boolean gameover = true;
-        int score = 5000;
+        int score = 4000;
         int levelCompleted = 5;
         int bonus = 1000;
 
-        if (score <== 5000)
-            System.out.println("Your score was 5000");
+        if (score < 5000 && score > 1000) {
+            System.out.println("Your score was less than 5000");
+        } else if (score < 1000) {
+            System.out.println("your score was less than 1000");
+        } else {
+            System.out.println(" Got here");
+        }
+
     }
 }
