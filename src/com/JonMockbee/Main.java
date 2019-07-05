74b30597561971c820485dd952fd49351c919c0b
Jon Mockbee <mockbee1000@gmail.com>
2019-07-05 10:43:47 -0400

    initial commit

---
+++ src/com/JonMockbee/Main.java
@@ -0,0 +1,14 @@
+package com.JonMockbee;
+
+public class Main {
+
+    public static void main(String[] args) {
+        boolean gameover = true;
+        int score = 5000;
+        int levelCompleted = 5;
+        int bonus = 1000;
+
+        if (score <== 5000)
+            System.out.println("Your score was 5000");
+    }
+}
